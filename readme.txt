d47d653c0251ef91fb6394bf567a9d3fe46b724a
rbtlong.com <rbtlong@live.com>
2020-02-16 22:19:46 -0800

    added ctrl helper for ctx

--- readme.txt
+++ readme.txt
@@ -1,4 +1,5 @@
 - Import this as a module (File -> New -> Module from existing source).
+- File -> Project Structure -> Modules -> (+) on the right -> Add Module Dependency -> select 'helperj'
 - Create a server.json file (use the server.json.example). It will look for the file in your launch path.
 - Add the following dependencies to your POM/Gradle...
     <dependency>
@@ -12,3 +13,8 @@
         <artifactId>gson</artifactId>
         <version>2.8.6</version>
     </dependency>
+- Make sure your compiler is set to version 1.8 or higher (pom.xml)...
+    <properties>
+        <maven.compiler.source>1.11</maven.compiler.source>
+        <maven.compiler.target>1.11</maven.compiler.target>
+    </properties>
---
+++ src/helperj/ctrl/Ctrl.java
@@ -0,0 +1,31 @@
+package helperj.ctrl;
+
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ctrl {
+    public static Map<String, Object> Result(Object content, double success) {
+        Map<String, Object> res = new HashMap<>();
+        res.put("content", content);
+        res.put("success", success);
+        return res;
+    }
+
+    public static void Res(Context ctx, Object content, double success, int status) {
+        Map<String, Object> res = Result(content, success);
+        ctx.status(status);
+        ctx.result(new Gson().toJson(res));
+    }
+
+    public static void Res(Context ctx, Object content, double success) {
+        Res(ctx, content, success, 200);
+    }
+
+    public static void Succ(Context ctx, Object content) {
+        Res(ctx, content, 1, 200);
+    }
+
+}
